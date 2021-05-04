40a3e9e05d3c59e4900bef72aabae897a03c1df8
Helton Reis <hrkings@rhaizzing.com>
2021-05-04 19:39:32 -0300

    [feature] Add ranger devicons installation to the install script

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ ln -sf ${PWD}/scripts/.zsh_aliases.sh ${HOME}/.zsh_aliases.sh
 # Ranger config ------------------------------------------------
 echo "Creating symlink to ranger config"
 ln -sf ${PWD}/config/rc.conf ${HOME}/.config/ranger/rc.conf
+echo "Installing ranger devicons"
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
 # Noto Color Emoji ------------------------------------------------
 echo "Creating symlink to ranger config"
