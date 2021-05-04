049e81cc0745fafd27727416ebfcb4f1eb517c22
Helton Reis <hrkings@rhaizzing.com>
2021-05-04 01:52:52 -0300

    [feature] Add Noto Color Emoji config

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ ln -sf ${PWD}/scripts/.zsh_aliases.sh ${HOME}/.zsh_aliases.sh
 # Ranger config ------------------------------------------------
 echo "Creating symlink to ranger config"
 ln -sf ${PWD}/config/rc.conf ${HOME}/.config/ranger/rc.conf
+
+# Noto Color Emoji ------------------------------------------------
+echo "Creating symlink to ranger config"
+ln -sf ${PWD}/config/99-noto-color-emoji.conf ${HOME}/.config/fontconfig/conf.d/99-noto-color-emoji.conf
