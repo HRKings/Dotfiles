68b248924ed53fa5d95d178d5dbf77240e0778e2
Helton Reis <hrkings@rhaizzing.com>
2021-05-05 00:18:37 -0300

    [change]{!!!} Move install config script to the scripts folder

--- scripts/install_configs.sh
+++ scripts/install_configs.sh
@@ -1,18 +1,21 @@
+# Set some variables ------------------------------------------------
+CONFIG_DIR=$(dirname $PWD)
+
 # ZSH Config ---------------------------------------------------
 echo "-- Creating symlink to zsh config --"
-ln -sf ${PWD}/config/.zshrc ${HOME}/.zshrc
+ln -sf ${CONFIG_DIR}/config/.zshrc ${HOME}/.zshrc
 
 # Aliases ------------------------------------------------------
 echo "-- Creating symlink to the aliases script --"
-ln -sf ${PWD}/scripts/.zsh_aliases.sh ${HOME}/.zsh_aliases.sh
+ln -sf ${CONFIG_DIR}/scripts/.zsh_aliases.sh ${HOME}/.zsh_aliases.sh
 
 # Ranger config ------------------------------------------------
 echo "-- Creating symlink to ranger config --"
-ln -sf ${PWD}/config/dotConfig/ranger/rc.conf ${HOME}/.config/ranger/rc.conf
+ln -sf ${CONFIG_DIR}/config/dotConfig/ranger/rc.conf ${HOME}/.config/ranger/rc.conf
 
 echo "-- Installing ranger devicons --"
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 
 # Noto Color Emoji ------------------------------------------------
 echo "-- Creating symlink to emoji config --"
-ln -sf ${PWD}/config/fontconfig/99-noto-color-emoji.conf ${HOME}/.config/fontconfig/conf.d/99-noto-color-emoji.conf
+ln -sf ${CONFIG_DIR}/config/fontconfig/99-noto-color-emoji.conf ${HOME}/.config/fontconfig/conf.d/99-noto-color-emoji.conf
