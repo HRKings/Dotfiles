23a7d6e220f2050b63c249edd0d9db1622cc54da
Helton Reis <hrkings@rhaizzing.com>
2021-05-11 23:34:38 -0300

    [feature] Add system wide cache cleanup script

---
+++ scripts/system_wide_cleanup.sh
@@ -0,0 +1,15 @@
+# Clear all docker storage --------------------------------
+docker system prune -a -f
+docker volume rm $(docker volume ls -qf dangling=true)
+
+# Clear all pacman cache, leaving only 1 version behind ---
+paccache -rk1
+
+# Clear all yay cache --
+yay -Sc
+
+# Clear all pacman cache ----
+pacman -Sc
+
+# Clear all nuget cache ----------
+dotnet nuget locals all --clear
