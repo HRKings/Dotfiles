630be048f2626bc9d9990d53e1bdf2b1998c8168
Helton Reis <hrkings@rhaizzing.com>
2021-05-13 23:39:06 -0300

    [style] Tab format all the things

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,70 +1,71 @@
 # Packages from the official Arch repository -----------------------------------------------------
 declare -a OFFICIAL_PKGS=(
 	# System
-	"bluez" "bluez-utils-compat"				   # Bluetooth support
-	"hunspell" "hunspell-en_US"					   # Spellchecking and the english dictionary
-	"kvantum-qt5"								   # SVG theme engine for KDE
-	"lshw"										   # To list the hardware
-	"gnome-keyring"								   # Some programs need the keyring to store auth data
+	"bluez" "bluez-utils-compat"					# Bluetooth support
+	"hunspell" "hunspell-en_US"						# Spellchecking and the english dictionary
+	"kvantum-qt5"									# SVG theme engine for KDE
+	"lshw"											# To list the hardware
+	"gnome-keyring"									# Some programs need the keyring to store auth data
+	"xclip"											# Clipboard manager for the command line
 
 	# Base 
-	"exa"										   # A better ls
-	"neovim"									   # The only vim
-	"ranger"									   # Terminal file explorer
-	"ueberzug"									   # To preview images in the terminal when using ranger
-	"trash-cli"									   # Send files to the DE trashbin instead of obliterating them
-	"antigen"									   # A plugin manager for ZSH
+	"exa"											# A better ls
+	"neovim"										# The only vim
+	"ranger"										# Terminal file explorer
+	"ueberzug"										# To preview images in the terminal when using ranger
+	"trash-cli"										# Send files to the DE trashbin instead of obliterating them
+	"antigen"										# A plugin manager for ZSH
 
 	# Development
-	"dotnet-runtime" "dotnet-sdk"				   # Make and run .NET code
+	"dotnet-runtime" "dotnet-sdk"					# Make and run .NET code
 
 	# Utils
-	"mpv"										   # The best universal media player
-	"qbittorrent"								   # My preferred torrent client
-	"thunderbird"								   # Email client
-	"qutebrowser"								   # An minimal and keyboard centered browser
-	"flameshot"									   # Screenshot tool
-	"libreoffice-fresh"							   # Because everyone needs to edit documents
-	"chromium"									   # A chromium engine is needed more times that you may think
-	"braus-git"									   # Ask which browser to use when opening each link
-	"jq"										   # A terminal JSON formatter
+	"mpv"											# The best universal media player
+	"qbittorrent"									# My preferred torrent client
+	"thunderbird"									# Email client
+	"qutebrowser"									# An minimal and keyboard centered browser
+	"flameshot"										# Screenshot tool
+	"libreoffice-fresh"								# Because everyone needs to edit documents
+	"chromium"										# A chromium engine is needed more times that you may think
+	"braus-git"										# Ask which browser to use when opening each link
+	"jq"											# A terminal JSON formatter
 
 	# Gaming
-	"vulkan-icd-loader" "lib32-vulkan-icd-loader"  # Vulkan libs required by some games
-	"steam"										   # One-stop game shop
-	"lutris"									   # For when "one-stop" is actually two-stops
-	"discord"									   # Communication at its finest
+	"vulkan-icd-loader" "lib32-vulkan-icd-loader"  	# Vulkan libs required by some games
+	"steam"											# One-stop game shop
+	"lutris"										# For when "one-stop" is actually two-stops
+	"discord"										# Communication at its finest
 
 	# Fonts
-	"ttf-roboto"								   # Roboto, a beautiful Google font
-	"noto-fonts-emoji"							   # OpenSource emoji font, because we like emojis, right ?
+	"ttf-roboto"									# Roboto, a beautiful Google font
+	"noto-fonts-emoji"								# OpenSource emoji font, because we like emojis, right ?
 )
 
 # Packages from AUR -------------------------------------------------------------------------------
 declare -a AUR_PKGS=(
 	# System
-	"hunspell-pt-br"				# Portuguese spellcheck
-	"epson-inkjet-printer-escpr"	# EPSON printer drivers
+	"hunspell-pt-br"					# Portuguese spellcheck
+	"epson-inkjet-printer-escpr"		# EPSON printer drivers
 
 	# Utils
-	"whatsapp-nativefier"		   # Because unforunally, discord isn't enough
-	"teams"						   # Because unforunally, discord isn't enough, part 2
-	"spotify"					   # Let's dance, baby
-	"keepassxc"					   # My favorite password manager
-	"stretchly"					   # Break reminder
-	"quiterss"					   # One of the best RSS feed readers
+	"whatsapp-nativefier"				# Because unforunally, discord isn't enough
+	"teams"								# Because unforunally, discord isn't enough, part 2
+	"spotify"							# Let's dance, baby
+	"keepassxc"							# My favorite password manager
+	"stretchly"							# Break reminder
+	"quiterss"							# One of the best RSS feed readers
 
 	# Development
-	"visual-studio-code-bin"	   # Proprietary VSCode, because we need things to function
-	"firefox-developer-edition"	   # My to-go browser, with lots of feature for developers
-	"jetbrains-toolbox"			   # Installs any jetbrains related product with one authentication
-	"insomnia"					   # REST API testing workspace
+	"visual-studio-code-bin"			# Proprietary VSCode, because we need things to function
+	"firefox-developer-edition"			# My to-go browser, with lots of feature for developers
+	"jetbrains-toolbox"					# Installs any jetbrains related product with one authentication
+	"insomnia"							# REST API testing workspace
 
 	# Customization
 	"spicetify-cli" "spicetify-custom-apps-and-extensions-git" "spicetify-themes-git" # Custom theme and extensions for the offical Spotify client
 
 	# Fonts
-	"nerd-fonts-fira-code"		  # FiraCode Nerd Font
+	"nerd-fonts-fira-code"		  		# FiraCode Nerd Font
 )
 
 # Instalation -------------------------------------------------------------------------------------
