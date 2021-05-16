3e9c9d348c7c387dbac6e44c896b077e430b5e36
Helton Reis <hrkings@rhaizzing.com>
2021-05-16 19:43:09 -0300

    [feature] Add new extensions for VSCode

--- scripts/install_vscode_extensions.sh
+++ scripts/install_vscode_extensions.sh
@@ -11,16 +11,29 @@ declare -a VSCODE_EXTENSIONS=(
 	"pkief.material-icon-theme"				# Material Icons
 	"zhuangtongfa.material-theme"			# OneDark Pro Theme
 
-	# Utilities -----------------------------------------------------
+	# Utilities -------------------------------------------------------------
 	"eamodio.gitlens"						# Gitlens
 	"ms-vsliveshare.vsliveshare"			# LiveShare
 	"coenraads.bracket-pair-colorizer-2"	# BracketPair Colorizer 2
 	"alefragnani.project-manager"			# Project Manager
 	"streetsidesoftware.code-spell-checker"	# Spell Checking
+	"wix.vscode-import-cost" 				# JS Package Import Cost
+	"WallabyJs.quokka-vscode" 				# Inline function results for JS
+
+	# Intellisense ------------------------------------------------------
+	"christian-kohler.npm-intellisense"		# Intellisense for NPM
+	"christian-kohler.path-intellisense"	# Intellisense for File Path
+	"octref.vetur"							# Vue Tooling
+
+	# Workflow ------------------------------------------------------
+	"formulahendry.auto-close-tag"			# Auto Close HTML Tags
+	"formulahendry.auto-rename-tag"			# Auto Rename HTML Tags
+	"fosshaas.fontsize-shortcuts"			# Add fontsize shortcuts
 	
 	# Linter/ Formatter ------------------------------
 	"esbenp.prettier-vscode"				# Prettier
 	"dbaeumer.vscode-eslint"				# ESLint
+	"HookyQR.beautify"						# Beautify JS, CSS, JSON files
 )
 
 # Install each extension --------------------------------------------
