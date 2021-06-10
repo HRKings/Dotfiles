fe7061329c3b5ac685315a9af3b4601c31207680
Helton Reis <hrkings@rhaizzing.com>
2021-06-09 23:54:23 -0300

    [change] Use dotbot to manage dotfiles

--- complete_install.sh
+++ complete_install.sh
@@ -4,16 +4,16 @@
 
 # Install all packages ---------------------
 echo "Installing all required packages..."
-source ${PWD}/scripts/install_packages.sh
+source ${PWD}/install_scripts/install_packages.sh
 
 # Install configs ---------------------
 echo "Installing all the configs..."
-source ${PWD}/install_configs.sh
+dotbot -c install.conf.yaml
 
 # Install VSCode extensions --------------------------
 echo "Installing all the required VSCode extensions"
-source ${PWD}/scripts/install_vscode_extensions.sh
+source ${PWD}/install_scripts/install_vscode_extensions.sh
 
 # Install .NET global tools --------------------------
 echo "Installing all the .NET global tools"
-source ${PWD}/scripts/install_dotnet_tools.sh
+source ${PWD}/install_scripts/install_dotnet_tools.sh
