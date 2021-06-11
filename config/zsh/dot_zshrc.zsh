d47aba181fec51d0fd4787c17cf1b41cf07e5521
Helton Reis <hrkings@rhaizzing.com>
2021-06-11 15:57:54 -0300

    [change]{!!!/ux} Change Antigen for ZInit

--- config/zsh/dot_zshrc.zsh
+++ config/zsh/dot_zshrc.zsh
@@ -9,14 +9,33 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 #==================================================================================================
-# Antigen (ZSH Plugins)
+# ZInit (ZSH Plugins)
 #==================================================================================================
 
-# Load Antigen --------------------------
-source /usr/share/zsh/share/antigen.zsh
+### Added by Zinit's installer
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
 
-# Load Antigen bundles ----
-antigen init ~/.antigenrc
+source "$HOME/.zinit/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+### End of Zinit's installer chunk
+
+zinit snippet OMZ::plugins/git/git.plugin.zsh           # Load OhMyZsh!
+zinit snippet PZT::modules/helper/init.zsh              # Load Prezto
+
+zinit light Aloxaf/fzf-tab					            # Use fzs for tab auto completion
+zinit light zsh-users/zsh-autosuggestions	            # Fish-like auto suggestions
+zinit light zsh-users/zsh-completions		            # Extra zsh completions
+zinit light zdharma/fast-syntax-highlighting	        # Syntax highlighting bundle
+zinit light agkozak/zsh-z					            # Z directory auto-jumping
+
+zinit ice depth=1; zinit light romkatv/powerlevel10k    # Custom prompt
 
 #==================================================================================================
 # Powerlevel10k Config 
@@ -24,13 +43,6 @@ antigen init ~/.antigenrc
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-#================================================================================================================================
-# General Configs
-#================================================================================================================================
-
-# Config keys -----------------------
-zstyle ':completion:*' list-colors
 
 #==================================================================================================
 # The rest of my .zshrc
