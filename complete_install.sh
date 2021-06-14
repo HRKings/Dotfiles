5a23217cb7272eae405e2fcaa4402d7b6c85df7d
Helton Reis <hrkings@rhaizzing.com>
2021-06-14 14:00:47 -0300

    [change]{rm} Update complete install script to remove zinit

--- complete_install.sh
+++ complete_install.sh
@@ -6,11 +6,6 @@
 echo "> Installing all required packages..."
 source ${PWD}/install_scripts/install_packages.sh
 
-# Install Zinit -----------------------------------------------------------------------------
-echo "> Installing ZInit..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-echo "After everything is done, run 'zinit self-update' in a new terminal to complete ZInit installation!"
-
 # Install configs ---------------------
 echo "> Installing all the configs..."
 dotbot -c install.conf.yaml
