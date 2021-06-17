81956d0015a1a1e2a8d19c31b0727e6dc3810a7e
Helton Reis <hrkings@rhaizzing.com>
2021-06-16 23:00:56 -0300

    [chore] Add comments to function

--- config/zsh/dot_zshd/functions.zsh
+++ config/zsh/dot_zshd/functions.zsh
@@ -11,10 +11,12 @@ reloadzsh() {
 	done
 }
 
+# List all packages in both Upstream and AUR, with fuzzy searching via fzf ----------------------------------------------------
 pkgfind() {
 	yay -Sl | awk '{print $2($4=="" ? "" : " *")}' | fzf --multi --preview 'yay -Si {1}' | cut -d " " -f 1 | xargs -ro yay -S
 }
 
+# Execute a Git command in all repositories contained in nested directories ----
 gitrecurse() {
 	for dir in $(find . -name ".git")
 	do 
@@ -25,6 +27,7 @@ gitrecurse() {
 	done
 }
 
+# Executes a diff in two strings ----------------------------------------
 stringdiff() {
 	echo $1 > /tmp/string_diff_file_1
 	echo $2 > /tmp/string_diff_file_2
