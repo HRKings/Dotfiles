a05ca644c1f3e706bd70a56a73e639eaf845a6f3
Helton Reis <hrkings@rhaizzing.com>
2021-06-20 02:08:05 -0300

    [change] Make the browser chooser a native Rofi script with support for icons

--- install_scripts/install_browser_chooser.sh
+++ install_scripts/install_browser_chooser.sh
@@ -2,6 +2,6 @@
 
 desktop-file-install --dir=$HOME/.local/share/applications misc/browser-chooser.desktop
 
-sed -r "4s;.*;Exec=/usr/bin/sh $HOME/.bin/browser_chooser.sh %U;" -i $HOME/.local/share/applications/browser-chooser.desktop
+sed -r "4s;.*;Exec=env URL=%U /usr/bin/rofi -modi 'bc:$HOME/.bin/browser_chooser.sh' -show bc;" -i $HOME/.local/share/applications/browser-chooser.desktop
 
 update-desktop-database ~/.local/share/applications
--- scripts/rofi/rofi_browser.sh
+++ scripts/rofi/rofi_browser.sh
@@ -1,22 +1,26 @@
-#!/bin/bash/
+#!/bin/bash
 
-# Gets all browser .desktop files
-BROWSER_DEKSTOP_FILES=$(grep 'x-scheme-handler/https' /usr/share/applications/mimeinfo.cache | awk -F "=" '{ print $2 }')
+# If its showing Rofi for the first time
+if [ $ROFI_RETV == "0" ];then
+	# Gets all browser .desktop files
+	BROWSER_DEKSTOP_FILES=$(grep 'x-scheme-handler/https' /usr/share/applications/mimeinfo.cache | awk -F "=" '{ print $2 }')
 
-# Transform the string into an array
-IFS=';' read -r -a BROWSER_DEKSTOP_FILES <<< $BROWSER_DEKSTOP_FILES
+	# Transform the string into an array
+	IFS=';' read -r -a BROWSER_DEKSTOP_FILES <<< $BROWSER_DEKSTOP_FILES
 
-# Initialize an key-value pair array to hold the browser names and exec paths
-declare -A BROWSERS
+	# For each browser desktop file, get the name and exec path
+	for desktop_file in "${BROWSER_DEKSTOP_FILES[@]}";
+	do
+		BROWSER_NAME=$(grep 'Name=' /usr/share/applications/$desktop_file | awk -F "=" '{ print $2 }' | head -n 1)
+		BROWSER_EXEC=$(grep 'Exec=' /usr/share/applications/$desktop_file | awk -F "Exec=" '{ print $2 }' | head -n 1)
+		BROWSER_ICON=$(grep 'Icon=' /usr/share/applications/$desktop_file | awk -F "Icon=" '{ print $2 }' | head -n 1)
+		echo -en "$BROWSER_NAME\0icon\x1f$BROWSER_ICON\x1finfo\x1f$BROWSER_EXEC\n"
+	done
+fi
 
-# For each browser desktop file, get the name and exec path, storing them on the key-value pair array
-for desktop_file in "${BROWSER_DEKSTOP_FILES[@]}";
-do
-	BROWSERS[$(grep 'Name=' /usr/share/applications/$desktop_file | awk -F "=" '{ print $2 }' | head -n 1)]=$(grep 'Exec=' /usr/share/applications/$desktop_file | awk -F "Exec=" '{ print $2 }' | head -n 1)
-done
-
-# Launch Rofi to present the user the browser name to choose
-CHOICE=$(printf '%s\n' "${!BROWSERS[@]}" | rofi -dmenu -i -p "Select the browser to open the link")
-
-# Strip the exec path from the '%U' or '%u' and launch with the provided URL
-$(echo ${BROWSERS[$CHOICE]} | sed "s/\%[Uu]//g")$1
+# If the user has choose one option
+if [ $ROFI_RETV == "1" ];then
+	# Strip the exec path from the '%U' or '%u' and launch with the provided URL
+	coproc ( $(echo $ROFI_INFO | sed "s/\%[Uu]//g")$URL  > /dev/null  2>&1 )
+	exit 0
+fi
