ecbd8e36cf128326ca9d965bbb072d8f1a288e83
Helton Reis <hrkings@rhaizzing.com>
2021-08-17 14:32:43 -0300

    [change] Change variables

--- config/zsh/dot_zshd/variables.zsh
+++ config/zsh/dot_zshd/variables.zsh
@@ -3,11 +3,13 @@
 #================================================================================================================================
 
 # .NET path config for Arch ------------------------------
-export PATH="$PATH:${HOME}/.dotnet/tools"
+#export PATH="$PATH:${HOME}/.dotnet/tools"
 export DOTNET_ROOT=$(dirname $(realpath $(which dotnet)))
 
 # External global tools ---------
 export PATH="$PATH:${HOME}/.bin"
 
+PATH=$PATH:/home/ton/Dev/Work/Rewrite/consultorio-dev-utilities/bash/scripts
+
 # Set default browser to Braus (Select broswer for each link) --
-export BROWSER=braus
+#export BROWSER=${HOME}/.bin/browser_chooser.sh
