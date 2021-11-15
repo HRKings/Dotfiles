8240a6f9f07551e5c5a6deca61630324f1ffbb12
Helton Reis <hrkings@rhaizzing.com>
2021-11-15 17:30:41 -0300

    [change] Reorganize PATH

--- config/zsh/dot_zshd/variables.zsh
+++ config/zsh/dot_zshd/variables.zsh
@@ -3,13 +3,11 @@
 #================================================================================================================================
 
 # .NET path config for Arch ------------------------------
-#export PATH="$PATH:${HOME}/.dotnet/tools"
+export PATH="$PATH:${HOME}/.dotnet:${HOME}/.dotnet/tools:${HOME}/.cargo/bin"
 export DOTNET_ROOT=$(dirname $(realpath $(which dotnet)))
 
 # External global tools ---------
 export PATH="$PATH:${HOME}/.bin"
-
-PATH=$PATH:/home/ton/Dev/Work/Rewrite/consultorio-dev-utilities/bash/scripts
 
 # Set default browser to Braus (Select broswer for each link) --
 #export BROWSER=${HOME}/.bin/browser_chooser.sh
