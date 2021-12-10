1e8a8d02cc37b130f423d2a03fe5a90edbb7ce53
Helton Reis <hrkings@rhaizzing.com>
2021-12-10 18:56:37 -0300

    [refactor] Cleanup one install script

--- install_scripts/install_browser_chooser.sh
+++ install_scripts/install_browser_chooser.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#! /usr/bin/bash
 
-desktop-file-install --dir=$HOME/.local/share/applications misc/browser-chooser.desktop
+desktop-file-install --dir="$HOME/.local/share/applications misc/browser-chooser.desktop"
 
-sed -r "4s;.*;Exec=env URL=%U /usr/bin/rofi -modi 'bc:$HOME/.bin/browser_chooser.sh' -show bc;" -i $HOME/.local/share/applications/browser-chooser.desktop
+sed -r "4s;.*;Exec=env URL=%U /usr/bin/rofi -modi 'bc:$HOME/.bin/browser_chooser.sh' -show bc;" -i "$HOME/.local/share/applications/browser-chooser.desktop"
 
 update-desktop-database ~/.local/share/applications
