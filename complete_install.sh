74a206a20127667ff2b3a8cf00ae41df22d88844
Helton Reis <hrkings@rhaizzing.com>
2022-01-04 00:05:40 -0300

    [feature] Add command to ZSH as default shell and a finishing message

--- complete_install.sh
+++ complete_install.sh
@@ -16,6 +16,10 @@ git clone https://github.com/HRKings/Private-Dotfiles.git .private
 echo "> Installing all the configs..."
 dotbot -c install.conf.yaml
 
+# Set ZSH as the default shell ----
+echo "> Setting ZSH as the default shell..."
+chsh -s /bin/zsh
+
 # Install VSCode extensions ------------------------------
 echo "> Installing all the required VSCode extensions..."
 source "${PWD}/install_scripts/install_vscode_extensions.sh"
@@ -37,3 +41,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # Install Ranger icons ----------------------------------------------------------------------------------
 echo "> Installing Ranger icons..."
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+
+# =========================================================================
+# Finish installation
+# =========================================================================
+
+echo "Installation complete, restart the terminal to load the new setup!"
