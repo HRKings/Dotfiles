3183543b4afb839ba5b1f5f43144145e0f175555
Helton Reis <hrkings@rhaizzing.com>
2022-01-05 17:13:31 -0300

    [misc]{rm} Remove browser chooser

--- install_scripts/install_browser_chooser.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-desktop-file-install --dir="$HOME/.local/share/applications misc/browser-chooser.desktop"
-
-sed -r "4s;.*;Exec=env URL=%U /usr/bin/rofi -modi 'bc:$HOME/.bin/browser_chooser.sh' -show bc;" -i "$HOME/.local/share/applications/browser-chooser.desktop"
-
-update-desktop-database ~/.local/share/applications
