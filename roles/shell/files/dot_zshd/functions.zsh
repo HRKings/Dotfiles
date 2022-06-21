33b00736ec6fcaa6001096f4e2dc2ceb2e6f18fd
Helton Reis <hrkings@rhaizzing.com>
2022-06-21 14:32:00 -0300

    [change] Get 100 gists for the mkcompose command

--- roles/shell/files/dot_zshd/functions.zsh
+++ roles/shell/files/dot_zshd/functions.zsh
@@ -40,7 +40,7 @@ function dumpgist {
     return
   fi
 
-  GIST=$(gh gist list | grep "Template Docker Compose File" | cut -f 1,2 | fzf --preview 'echo {1} | cut -f 1 | xargs gh gist view --raw ')
+  GIST=$(gh gist list -L 100 | grep "Template Docker Compose File" | cut -f 1,2 | fzf --preview 'echo {1} | cut -f 1 | xargs gh gist view --raw ')
 
   if [[ ! -z "$GIST" ]]; then
     gh gist view --raw "$(echo $GIST | cut -f 1)" | tail -n +3 > $1 \
