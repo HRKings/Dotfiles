6819719f62bf16f5aa3a388b1dbb56f4d126fe42
Helton Reis <hrkings@rhaizzing.com>
2022-07-07 12:27:21 -0300

    [misc] Add cool init to every shell session

---
+++ roles/shell/files/dot_zshd/session_init.zsh
@@ -0,0 +1,17 @@
+function aperture_science_quote {
+    APERTURE_SCIENCE=("I'll be honest, we're throwing science at the wall here to see what sticks. No idea what it'll do. Probably nothing."
+                      "Science isn't about WHY. It's about WHY NOT."
+                      "For science... We do what we MUST! Because we can."
+                      "Who is ready to make some science?"
+                      "Bold, persistent experimentation is the hallmark of good science.")
+
+    selected_quote=$(shuf -n 1 -e "$APERTURE_SCIENCE[@]")
+
+    echo $selected_quote
+}
+
+function cool_init {
+  aperture_science_quote | pokemonsay -W $(tput cols)
+}
+
+cool_init
