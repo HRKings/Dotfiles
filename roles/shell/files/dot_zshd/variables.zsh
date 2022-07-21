89c013bfa665d251e7ff1eed50273275ab0ffc26
Helton Reis <hrkings@rhaizzing.com>
2022-07-20 21:48:44 -0300

    feat(shell/variables): update JAVA variables

--- roles/shell/files/dot_zshd/variables.zsh
+++ roles/shell/files/dot_zshd/variables.zsh
@@ -13,7 +13,17 @@ export GOPATH="${HOME}/go"
 export PATH="$PATH:${HOME}/.local/bin:${GOPATH}/bin:${HOME}/.cargo/bin"
 
 # Enable Anti Aliasing for fonts inside Java GUI aplications ---
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'
+export _JAVA_OPTIONS='-Dswing.aatext=TRUE -Dawt.useSystemAAFontSettings=on'
 
 # WakaTime CLI -----------------------------
 export ZSH_WAKATIME_BIN='/usr/bin/wakatime'
+
+#================================================================================================================================
+# ZStyle configs
+#================================================================================================================================
+
+# Configure ZSH Notify ------------------------------
+zstyle ':notify:*' command-complete-timeout 1
+zstyle ':notify:*' error-title "Command failed"
+zstyle ':notify:*' success-title "Command finished"
+zstyle ':notify:*' expire-time 15000
