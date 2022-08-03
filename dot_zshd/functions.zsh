234735690fd13c216d2d7fc6698fc5b69b5a771f
Helton Reis <hrkings@rhaizzing.com>
2022-08-02 23:20:29 -0300

    feat(shell): add function to call nnn

--- dot_zshd/functions.zsh
+++ dot_zshd/functions.zsh
@@ -278,3 +278,40 @@ view7zfiles() {
 	&& printf '\n$DIVIDER\n' \
 	&& 7z l {} | tail -n +21 | head -n -2 | awk  'BEGIN{FIELDWIDTHS=\"53 1000\"}{print \$2}' | tree --fromfile ."
 }
+
+# Open nnn with cd on quit and the preview plugin --------------------------------------
+n ()
+{
+    # Block nesting of nnn in subshells
+    if [[ "${NNNLVL:-0}" -ge 1 ]]; then
+        echo "nnn is already running"
+        return
+    fi
+
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "export" and make sure not to
+    # use a custom path, i.e. set NNN_TMPFILE *exactly* as follows:
+    #     NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+		export PAGER="less -R"
+		# a = file preview requirement; r = Use advcpmv
+		export NNN_OPTS="ar"
+		export NNN_PLUG='v:preview-tui'
+
+    # The backslash allows one to alias n to nnn if desired without making an
+    # infinitely recursive alias
+    \nnn -P v "$@"
+
+    if [ -f "$NNN_TMPFILE" ]; then
+            . "$NNN_TMPFILE"
+            rm -f "$NNN_TMPFILE" > /dev/null
+    fi
+}
