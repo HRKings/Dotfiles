195d2ab7247748a9874bcf0294ccf05b92cf5c1b
Helton Reis <hrkings@rhaizzing.com>
2024-03-26 15:23:06 -0300

    feat: update cargo installed tools

--- .chezmoiscripts/run_once_install-cargo-tools.sh
+++ .chezmoiscripts/run_once_install-cargo-tools.sh
@@ -1,12 +1,26 @@
 #! /bin/bash
 
 cargo_tools=(
-	evcxr_repl 			# Rust REPL
-	cargo-update		# Update Cargo packages
-	cargo-edit			# Edit the Cargo.toml easily
-	biodiff					# Diff hexdumps using biological alignment algorithms
-	evcxr_repl			# Rust REPL
+	ast-grep             # Grep but for code (support AST searching and replacing)
+	biodiff              # Diff hexdumps using biological alignment algorithms
+	cargo-cache          # Display information on the cargo cache
+	cargo-clean-all      # Clean the target directories from various Rust projects
+	cargo-edit           # Edit the Cargo.toml easily
+	cargo-expand         # Show the results of a macro expansion
+	cargo-generate       # Create Rust projects using templates
+	cargo-information    # Show information of a crate
+	cargo-nextest        # A better test runner for Rust
+	cargo-quickinstall   # A fallback fro binstall
+	cargo-show-asm       # View the ASM output of a compiled function
+	cargo-update         # Update Cargo packages
+	coreutils            # GNU core utilities rewritten in Rust
+	evcxr_jupyter        # Rust Jupyter notebook support
+	fnm                  # Fast Node Manager, because having just one circle of hell isn't enough
+	irust                # Rust REPL
+	jwt-cli              # Fast JWL encoding and decoding
+	pest-language-server # LSP for Pest parser language
+	rnr                  # Rename multiple files on a directory with support for regex
 )
 
 # Install the packages ----------
-cargo install "${cargo_tools[@]}"
+cargo binstall "${cargo_tools[@]}"
